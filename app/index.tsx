c3bc8932fd9cd9599e87d986a2e3dd8e0ea9fa3b
Nir Gluzman <nir.gluzman@gmail.com>
2024-11-27 00:05:41 +0100

    add: todo operations and addTodo field

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,89 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { useState } from 'react';
+
+import { data } from '@/data/todos';
 
 export default function Index() {
+  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id)); // sorting the data array in descending order based on the id property.
+  const [text, setText] = useState('');
+
+  // add a new todo
+  const addTodo = () => {
+    if (text.trim()) {
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      setTodos([{ id: newId, title: text, completed: false }, ...todos]);
+      setText(''); // clear the input field after adding a todo.
+    }
+  };
+
+  // toggle the completed status
+  const toggleTodo = (id: number) => {
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+    );
+  };
+
+  // delete a todo
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='add a new todo'
+          placeholderTextColor='gray'
+          autoComplete={'off'} // disable auto-complete suggestions
+          autoCorrect={false} // disable auto-correct suggestions
+          value={text}
+          onChangeText={setText}
+        />
+        <Pressable style={styles.addButton} onPress={addTodo}>
+          <Text style={styles.addButtonText}>Add</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: 'black',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    padding: 10,
+    width: '100%',
+    maxWidth: 1024, // iPad Pro size
+    marginHorizontal: 'auto',
+    pointerEvents: 'auto', // control whether the View can be the target of touch events.
+  },
+  input: {
+    flex: 1,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    fontSize: 18,
+    minWidth: 0,
+    color: 'white',
+  },
+  addButton: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+  },
+  addButtonText: {
+    fontSize: 18,
+    color: 'black',
+  },
+});
