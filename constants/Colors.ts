75c5d8643bfc39103a252c84373b6919b476c5a1
Nir Gluzman <nir.gluzman@gmail.com>
2024-11-28 15:24:03 +0100

    add: Colors palette

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+// Colors palette
+
+export const Colors = {
+  // light theme
+  light: {
+    text: 'black',
+    background: 'white',
+    icon: 'black',
+    button: 'royalblue',
+  },
+
+  // dark theme
+  dark: {
+    text: 'white',
+    background: 'black',
+    icon: 'red',
+    button: 'white',
+  },
+};
